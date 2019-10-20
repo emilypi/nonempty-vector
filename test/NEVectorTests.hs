4da1894ef6c386f096ab8893bc7254f42a943b51
Emily Pillmore <emily@kadena.io>
2019-10-20 14:06:05 -0400

    add some sanity to constructors

--- test/NEVectorTests.hs
+++ test/NEVectorTests.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
 -- |
 -- Module       : Main (tests)
--- Copyright 	: 2019 Emily Pillmore
+-- Copyright 	: 2019 (c) Emily Pillmore
 -- License	: BSD
 --
 -- Maintainer	: Emily Pillmore <emilypi@cohomolo.gy>
@@ -9,14 +11,58 @@
 --
 module Main
 ( main
+, genNEV
 ) where
 
 
-import Test.QuickCheck
-
+import Data.Functor
+import Data.Maybe
+import Data.Vector (Vector)
 import qualified Data.Vector as V
+import Data.Vector.NonEmpty (NonEmptyVector)
 import qualified Data.Vector.NonEmpty as NEV
+
+import Hedgehog
+import qualified Hedgehog.Range as Range
+
+import qualified Hedgehog.Internal.Gen as Gen
+import qualified Hedgehog.Internal.Shrink as Shrink
+import Hedgehog.Internal.Source (HasCallStack, withFrozenCallStack)
+import Hedgehog.Internal.Tree (Tree)
+import qualified Hedgehog.Internal.Tree as Tree
 
 
 main :: IO ()
-main = return ()
+main = void $ checkParallel $ Group "NonEmptyVector constructor"
+    [ ("prop_reverse", prop_reverse)
+    , ("prop_from_to_list", prop_from_to_list)
+    , ("prop_from_to_vec", prop_from_to_vec)
+    ]
+
+genList :: Gen [Int]
+genList = Gen.list (Range.linear 1 100) Gen.enumBounded
+
+genNEV :: Gen (NonEmptyVector Int)
+genNEV = fmap (fromJust . NEV.fromList) genList
+
+genV :: Gen (Vector Int)
+genV = NEV.toVector <$> genNEV
+
+prop_reverse :: Property
+prop_reverse = property $ do
+  t <- forAll genNEV
+  NEV.reverse (NEV.reverse t) === t
+
+prop_from_to_list :: Property
+prop_from_to_list = property $ do
+    t <- forAll $ genNEV
+    u <- forAll $ genList
+    NEV.fromList (NEV.toList t) === Just t
+    fmap NEV.toList (NEV.fromList u) === Just u
+
+prop_from_to_vec :: Property
+prop_from_to_vec = property $ do
+    t <- forAll $ genNEV
+    u <- forAll $ genV
+    NEV.fromVector (NEV.toVector t) === Just t
+    fmap NEV.toVector (NEV.fromVector u) === Just u
